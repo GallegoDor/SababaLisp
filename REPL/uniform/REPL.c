97ad5c5d8acc576155ca712be5096f6a60900b14
Dor Gallego <dor.gallego@gmail.com>
2014-12-25 22:19:49 -0500

    Update REPL.c
    began parsing process.

--- REPL/uniform/REPL.c
+++ REPL/uniform/REPL.c
@@ -8,6 +8,7 @@ Licensed under GPL Affero 3.0 license.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "mpc.h"
 
 //For windows compilation
 #ifdef _WIN32
@@ -39,13 +40,45 @@ void add_history(char* unused){}
 #endif
 
 int main(int argc, char** argv){
-  puts("Sababa Version: 0.0.0.1");
+  //start of REGEX code for defining Sababa's grammer
+  /* Parser creation */
+  mpc_parser_t* Integer   = mpc_new("integer");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expression = mpc_new("expression");
+  mpc_parser_t* Sababa = mpc_new("sababa");
+
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                           \
+  integer    : /-?[0-9]+/ ;                               \
+  operator   : '+' | '-' | '*' | '/' ;                        \
+  expression : <integer> | '(' <operator> <expression>+ ')' ; \
+  sababa     : /^/ <operator> <expression>+ /$/ ;             \
+  ",
+  Integer, Operator, Expression, Sababa);
+  //end of REGEX code to define Sababa's grammer
+  puts("Sababa Version: 0.0.0.2");
   puts("Press ctrl+c to exit");
 
   while(true){ //keep prompting the user in a similar fashion
     char* input = readline("Sababa> ");
     add_history(input);
-    printf("%s\n", input);
+
+    /* Attempt to Parse the user Input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Sababa, &r)) {
+      /* If successful it'll print the abstract syntax tree */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise it'll print an error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
     free(input);
-  } return 0;
+  }
+
+  /* Get rid of the parsers */
+  mpc_cleanup(4, Integer, Operator, Expression, Sababa);
+  return 0;
 }
