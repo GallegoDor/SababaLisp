6e4b383f392da4a5e83a4fda50c910614bb559bd
Dor Gallego <dor.gallego@gmail.com>
2014-12-26 16:20:30 -0500

    Update REPL.c
    added max/min as well as basic arithmetic error handling

--- REPL/uniform/REPL.c
+++ REPL/uniform/REPL.c
@@ -11,6 +11,10 @@ Licensed under the Apache License v2.0
 #include <math.h>
 #include "mpc.h"
 
+//min and max functions for later use, not built into C
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
 //For windows compilation
 #ifdef _WIN32
 
@@ -39,6 +43,66 @@ void add_history(char* unused){}
 #include <editline/readline.h>
 #endif
 
+//Error Handling Portion
+/* Create Enumeration of Possible Error Types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create Enumeration of Possible lval Types (lval = lisp value) */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Declare New lval Struct */
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+/* Create a new number type lval */
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+/* Create a new error type lval */
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+/* Print an "lval" */
+void lval_print(lval v) {
+  switch (v.type) {
+    /* In the case the type is an integer, print it */
+    /* Then 'break' out of the switch. */
+    case LVAL_NUM: printf("%li", v.num); break;
+
+    /* In the case the type is an error */
+    case LVAL_ERR:
+    /* Check what type of error it is and print it */
+    if (v.err == LERR_DIV_ZERO) {
+      printf("Error: Division By Zero!");
+    }
+    if (v.err == LERR_BAD_OP)   {
+      printf("Error: Invalid Operator!");
+    }
+    if (v.err == LERR_BAD_NUM)  {
+      printf("Error: Invalid Number!");
+    }
+    break;
+  }
+}
+
+/* Print an "lval" followed by a newline */
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+//End error Handling portion
+
+//Start arimethic code, and language grammar
+
 /*  expression is *10 (+ 1 51) How it's represented in ast
 regex
 operator|char:1:1 '*'               <-- operator
@@ -54,28 +118,48 @@ regex
 
 /* Uses strcmp to check for operator, return 0 if true */
 // then evaluates x and y arguments
-long eval_op(long x, char* op, long y) {
-  if (strcmp(op, "+") == 0) { return x + y;    }
-  if (strcmp(op, "-") == 0) { return x - y;    }
-  if (strcmp(op, "*") == 0) { return x * y;    }
-  if (strcmp(op, "/") == 0) { return x / y;    }
-  if (strcmp(op, "%") == 0) { return x % y;    }
-  if (strcmp(op, "^") == 0) { return pow(x,y); }
-  return 0;
+lval eval_op(lval x, char* op, lval y) {
+
+  /* If either value is an error return it */
+  if (x.type == LVAL_ERR) { return x; }
+  if (y.type == LVAL_ERR) { return y; }
+
+  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num);    }
+  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num);    }
+  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num);    }
+  if (strcmp(op, "^") == 0) { return lval_num(pow(x.num,y.num)); }
+
+  if (strcmp(op, "/") == 0) {
+    /* If second operand is zero return error */
+    return y.num == 0
+    ? lval_err(LERR_DIV_ZERO)
+    : lval_num(x.num / y.num);
+  }
+
+  if (strcmp(op, "%") == 0) {
+    /* Same reasoning as above */
+    return y.num == 0
+    ? lval_err(LERR_DIV_ZERO)
+    : lval_num(x.num % y.num);
+  }
+  return lval_err(LERR_BAD_OP);
 }
 
 //function to obtain numbers to evaluate
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
   /* If tagged as integer return it directly. */
   if (strstr(t->tag, "integer")) {
-    return atoi(t->contents); //turn string contents into a long
+    /* Check if there is some error in conversion */
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   /* The operator is always second child. Since first is always '(' */
   char* op = t->children[1]->contents;
 
   /* We store the third child in `x` */
-  long sum = eval(t->children[2]);
+  lval sum = eval(t->children[2]);
 
   /* Iterate the remaining children and operate on them accordingly. */
   int i = 3;
@@ -85,6 +169,8 @@ long eval(mpc_ast_t* t) {
   }
   return sum;
 }
+
+//end math operations functionality, below syntax and grammar is defined
 
 int main(int argc, char** argv){
   //start of REGEX code for defining Sababa's grammer
@@ -104,7 +190,7 @@ int main(int argc, char** argv){
   ",
   Integer, Operator, Expression, Sababa);
   //end of REGEX code to define Sababa's grammer
-  puts("Sababa Version: 0.0.0.3");
+  puts("Sababa Version: 0.0.0.4");
   puts("Press ctrl+c to exit");
 
   while(true){ //keep prompting the user in a similar fashion
@@ -115,8 +201,8 @@ int main(int argc, char** argv){
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Sababa, &r)) {
       /* If successful it'll print the result of the calculation */
-      long result = eval(r.output);
-      printf("%li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise it'll print an error */
